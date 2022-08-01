f05e7336646ee33958f2bde6a66588d010106305
Kelvin Anderson <kelvinanders17@gmail.com>
2022-08-01 17:25:58 +0700

    products with hoc toggle

---
+++ src/features/product/ProductList.js
@@ -0,0 +1,15 @@
+const ProductList = (props) =>{
+    return(
+        <div>
+            <div>
+                <h2>Product</h2>
+            </div>
+            <div>
+                {props.data.map((p) => <ProductCard key = {p.productId} product={p}/>)}
+            </div>
+        </div>
+    
+    )
+}
+
+export default ProductList;
